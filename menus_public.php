9c8383459643259bc1dce98fd43712ac62a550ad
juliariciardi <j.segolin@campus.fct.unl.pt>
2025-05-14 18:46:46 +0100

    Create menus_public.php

---
+++ menus_public.php
@@ -0,0 +1,8 @@
+<div class="menu">
+    <a href="index.php">Home</a>
+    <a href="#">Serviços</a>
+    <a href="#">Equipa</a>
+    <a href="showlogin.php">Utilizadores</a>
+    <a href="showlogin.php">Login</a>
+    <a href="#">Contactos</a>
+</div>
