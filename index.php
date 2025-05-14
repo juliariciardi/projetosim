4c8008d0f4918f38246f0692783de9a6e69a16f4
juliariciardi <j.segolin@campus.fct.unl.pt>
2025-05-14 18:46:53 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php include ("header.php"); ?>
+<div class="main-content">
+    <div class="section servicos" id="serviços">
+        <h2>Serviços</h2>
+        <ul>
+            <li>Consultas</li>
+            <li>Exames</li>
+            <li>Análises</li>
+            <li>Apoio ao diagnóstico avançado</li>
+        </ul>
+    </div>
+
+    <div class="section" id="contatos">
+        <h2>Contatos</h2>
+        <p>
+            Clínica SIM<br>
+            Campus da FCT<br>
+            2820-516 Caparica
+        </p>
+    </div>
+    <div class="content">
+        <div class="section" id="equipa">
+            <h2>Equipa</h2>
+            <ul>
+                <li>Doutor André</li>
+                <li>Doutor José</li>
+                <li>Doutor Mora</li>
+                <li>Doutor Fonseca</li>
+            </ul>
+        </div>
+    </div>
+</div>
+<?php include ("footer.php"); ?>
