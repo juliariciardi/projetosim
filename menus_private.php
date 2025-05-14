1e5600a1be0ad5a109d8692c86e6241af9bbf2db
juliariciardi <j.segolin@campus.fct.unl.pt>
2025-05-14 18:46:43 +0100

    Create menus_private.php

---
+++ menus_private.php
@@ -0,0 +1,8 @@
+<div class="menu">
+    <a href="index.php">Home</a>
+    <a href="#">Serviços</a>
+    <a href="#">Equipa</a>
+    <a href="listusers.php">Utilizadores</a>
+    <a href="logout.php">Logout</a>
+    <a href="#">Contactos</a>
+</div>
